f5fecb1883024fa1cfbc56a7a37bd6e7d4ad3193
Ítalo Carmo <italo_rj@hotmail.com>
2022-04-12 11:45:50 -0300

    incluido /api na rota

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :heroes
+  #define uma rota /api antes dos controllers
+  namespace :api, defaults: { format: json } do
+    resources :heroes
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
